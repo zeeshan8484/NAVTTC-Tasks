7bb30ac607f2928c7f8bb6f17536fb986667c3c4
first commit <akramzeeshan837@gmail.com>
2026-05-31 23:53:32 +0500

    Initial commit

---
+++ week1/task1.py
@@ -0,0 +1,5 @@
+num1 = 1.5
+num2 = 6.3
+
+sum = num1 + num2
+print("The sum of", num1, "and", num2, "is", sum)
---
+++ week1/task2.py
@@ -0,0 +1,5 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+sum = float(num1) + float(num2)
+print("The sum of", num1, "and", num2, "is", sum)
